cc09ed8570bcea0c673738b6928603020f8bb3ff
Svetlana Zhikina <svetlana@zhikaweb.space>
2021-12-17 16:16:10 +0300

    Тест-версия

--- lib/utils/httpUtils.dart
+++ lib/utils/httpUtils.dart
@@ -1,9 +1,9 @@
 class HttpUtils {
 
   // PROD
-  static String host = 'portal.eapo.org';
+  // static String host = 'portal.eapo.org';
 
   // TEST
-  // static String host = 'portal8.eapo.org';
+  static String host = 'portal8.eapo.org';
   static String mainUrl = 'https://' + host + '/ws-v2/rest/';
 }
